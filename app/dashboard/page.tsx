866d91487e38868572c7ca2216e699a27f44517f
shakthi <shakthi@edumerge.com>
2025-07-05 13:10:33 +0530

    code corrected

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,62 +1,49 @@
-import BlogPost from "@/components/BlogPost"
-import { buttonVariants } from "@/components/ui/button"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import type { BlogPost as BlogPostType } from "@prisma/client"
-import Link from "next/link"
-import { redirect } from "next/navigation"
-import { prisma } from "../utils/db"
 
+import BlogPost from "@/components/BlogPost";
+import { buttonVariants } from "@/components/ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import { prisma } from "../utils/db";
 
-const  getData = async(userId:string)=>{
-     const data = await prisma.blogPost.findMany({
-      where:{
-        authorId:userId
-      },
-      orderBy:{
-        createdAt:'desc'
-      }
-     })
-     return data
+async function getData(userId: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = await prisma.blogPost.findMany({
+    where: {
+      authorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
 }
 
-const DashboardRoute = async() => {
-    const {getUser} = getKindeServerSession()
+export default async function DashboardRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    const  user = await getUser()
-    if (!user) {
-    throw new Error("User not authenticated")
-}
+  if (!user?.id) {
+    // You can redirect, throw, or return a message here
+    return <div>User not found.</div>;
+  }
+  const data = await getData(user?.id);
 
-const data: BlogPostType[] = await getData(user?.id);
-
-
-
-    if(!user){
-        return redirect("/api/auth/register")
-    }
   return (
     <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
 
-  
-    <div className="flex items-center justify-between mb-4 px-5">
-          <h2 className="text-xl font-medium">Your Blog Articles</h2>
+        <Link className={buttonVariants()} href="/dashboard/create">
+          Create Post
+        </Link>
+      </div>
 
-          <Link href='/dashboard/create' className={buttonVariants({variant:'blue'})}>
-              Create Post
-          </Link>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <BlogPost data={item} key={item.id} />
+        ))}
+      </div>
     </div>
-
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-               {
-                data.map((item: BlogPostType)=>{
-                  return <BlogPost  data={item} key={item.id}/>
-                
-                })
-               }
-    </div>
-  </div>
-   
-  )
+  );
 }
-
-export default DashboardRoute
